8348e182813f8b8d1fd8d4ff7d42081742a45717
hancong03 <121371790@qq.com>
2013-08-08 15:35:20 +0800

    前背景色

--- _src/adapter/font.js
+++ _src/adapter/font.js
@@ -114,51 +114,49 @@ UE.registerUI('fontfamily fontsize', function( name ) {
 
 UE.registerUI( 'forecolor backcolor', function( name ) {
 
-        var me = this,
-            colorPickerWidget = null,
-            fontIcon = null,
-            $btn = null;
+    var me = this,
+        colorPickerWidget = null,
+        fontIcon = null,
+        $btn = null;
 
-        //querycommand
-        this.addListener('selectionchange',function(){
+    //querycommand
+    this.addListener('selectionchange',function(){
 
-            var state = this.queryCommandState( name );
-            $btn.edui().disabled( state == -1 ).active( state == 1 );
+        var state = this.queryCommandState( name );
+        $btn.edui().disabled( state == -1 ).active( state == 1 );
 
-        });
+    });
 
-        $btn = $.eduisplitbutton({
-            icon: 'font',
-            caret: true,
-            title: me.getLang("labelMap")[name],
-            click: function() {
-                me.execCommand( name, getCurrentColor() );
-            }
-        });
-
-        fontIcon = $btn.find(".icon-font");
-
-        colorPickerWidget = $.eduicolorpicker({
-            lang_clearColor: me.getLang('clearColor') || '',
-            lang_themeColor: me.getLang('themeColor') || '',
-            lang_standardColor: me.getLang('standardColor') || ''
-        }).eduitablepicker( "attachTo", $btn ).edui().on('pickcolor', function( evt, color ){
-                window.setTimeout( function(){
-                    fontIcon.css("color", color);
-                    me.execCommand( name, color );
-                }, 0 );
-            });
-        colorPickerWidget.on('show',function(){
-            UE.setActiveWidget(colorPickerWidget.root())
-        });
-        $btn.edui().mergeWith( colorPickerWidget.root() );
-
-        return $btn;
-
-        function getCurrentColor() {
-            return domUtils.getComputedStyle( fontIcon[0], 'color' );
+    $btn = $.eduisplitbutton({
+        icon: 'font',
+        caret: true,
+        title: me.getLang("labelMap")[name],
+        click: function() {
+            me.execCommand( name, getCurrentColor() );
         }
+    });
 
+    fontIcon = $btn.find(".icon-font");
+
+    colorPickerWidget = $.eduicolorpicker({
+        lang_clearColor: me.getLang('clearColor') || '',
+        lang_themeColor: me.getLang('themeColor') || '',
+        lang_standardColor: me.getLang('standardColor') || ''
+    }).eduitablepicker( "attachTo", $btn ).edui().on('pickcolor', function( evt, color ){
+            window.setTimeout( function(){
+                fontIcon.css("color", color);
+                me.execCommand( name, color );
+            }, 0 );
+        });
+    colorPickerWidget.on('show',function(){
+        UE.setActiveWidget(colorPickerWidget.root())
+    });
+    $btn.edui().mergeWith( colorPickerWidget.root() );
+
+    return $btn;
+
+    function getCurrentColor() {
+        return domUtils.getComputedStyle( fontIcon[0], 'color' );
     }
 
-);
+} );
